bf5be96f69e3ac80c79f15056b226d4b8c89d972
Alexander Nguyen <nguyenalexander@Alexanders-MacBook-Pro.local>
2015-03-10 19:52:14 -0700

    added code to the functions getPlayerMove and getComputerMove

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,17 +22,15 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    var move;
+    move = getInput() || move;
+    return getPlayerMove;
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    var move;
+    move = randomPlay() || move;
+    return getComputerMove;
 }
 
 function getWinner(playerMove,computerMove) {
